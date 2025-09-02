e3f18007a2a28de404c2dc3a29b5beb09667b0c5
wawanneutron <wawanneutron1331@gmail.com>
2025-09-02 11:04:37 +0700

    feat: global styles

---
+++ src/styles/CardStyles.ts
@@ -0,0 +1,7 @@
+import { css } from 'styled-components'
+
+export const box = css`
+  background-color: var(--color-grey-0);
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-md);
+`
