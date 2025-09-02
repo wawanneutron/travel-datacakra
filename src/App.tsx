f18f962b9974b452069e13e7e60b3941a9a9db35
wawanneutron <wawanneutron1331@gmail.com>
2025-09-02 11:00:16 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>HELLO WORLD!</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore ut
+        reiciendis voluptates ipsam est in temporibus ad, quis, ducimus maxime
+        sunt natus numquam quam nam sequi voluptas. Maiores, corporis molestias.
+      </p>
+    </div>
+  )
+}
+
+export default App
