1c6deb86f9473ffe3016605bb9d6506556bf8f83
wawanneutron <wawanneutron1331@gmail.com>
2025-09-04 01:50:42 +0700

    chore: setup tanstack, query devtools, toaster and protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'react-hot-toast'
+
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -8,10 +12,22 @@ import DashboardCategory from './pages/dashboard/Category'
 import DashboardHome from './pages/dashboard/Home'
 import AppLayout from './ui/AppLayout'
 import AppLayoutDashboard from './ui/AppLayoutDashboard'
+import ProtectedRoute from './features/auth/ProtectedRoute'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      //note: set 5 menit auto fetch
+      staleTime: 1000 * 60 * 5
+    }
+  }
+})
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
@@ -23,14 +39,41 @@ function App() {
             <Route path="*" element={<div>Not Found</div>} />
           </Route>
 
-          <Route element={<AppLayoutDashboard />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayoutDashboard />
+              </ProtectedRoute>
+            }
+          >
             <Route path="dashboard" element={<DashboardHome />} />
             <Route path="dashboard/article" element={<DashboardArticle />} />
             <Route path="dashboard/category" element={<DashboardCategory />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: {
+            duration: 5000
+          },
+          error: {
+            duration: 5000
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: '#fff',
+            color: '#2C3D4F'
+          }
+        }}
+      />
+    </QueryClientProvider>
   )
 }
 
