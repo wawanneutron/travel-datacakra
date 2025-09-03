37f9b2afc72f527cef0e570915544e9ed04f1223
wawanneutron <wawanneutron1331@gmail.com>
2025-09-03 09:21:41 +0700

    ui: register

---
+++ src/pages/Register.tsx
@@ -0,0 +1,16 @@
+import FormRegister from '../features/register/FormRegister'
+import Logo from '../ui/Logo'
+
+export default function Login() {
+  return (
+    <section className="h-[80vh] m-auto flex flex-col items-center justify-center">
+      <div className="bg-primary-900 p-10">
+        <div className="flex flex-col items-center gap-2 mb-4 border-b py-4 border-primary-800">
+          <Logo label={false} />
+          <h3 className="font-mono text-3xl">Sign Up</h3>
+        </div>
+        <FormRegister />
+      </div>
+    </section>
+  )
+}
