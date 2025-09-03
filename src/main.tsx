a5439573492098b5b7482f8445dd8bd58396659d
wawanneutron <wawanneutron1331@gmail.com>
2025-09-04 01:51:09 +0700

    chore: setup redux provider and store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import App from './App.tsx'
+import store from './store/index.ts'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { Provider } from 'react-redux'
 import './index.css'
+import LoadAuth from './features/auth/LoadAuth.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <LoadAuth />
+      <App />
+    </Provider>
   </StrictMode>
 )
