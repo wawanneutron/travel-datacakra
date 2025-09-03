225f9ee133fa0ae10c2192a67ba900f6634b16f0
wawanneutron <wawanneutron1331@gmail.com>
2025-09-03 09:20:48 +0700

    feat: router app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import GlobalStyles from './styles/GlobalStyles'
 import Home from './pages/Home'
-import AppLayout from './ui/AppLayout'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import Travel from './pages/Travel'
+import DashboardArticle from './pages/dashboard/Article'
+import DashboardCategory from './pages/dashboard/Category'
+import DashboardHome from './pages/dashboard/Home'
+import AppLayout from './ui/AppLayout'
+import AppLayoutDashboard from './ui/AppLayoutDashboard'
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
             <Route index element={<Home />} />
             <Route path="travel-list" element={<Travel />} />
+
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
             <Route path="*" element={<div>Not Found</div>} />
+          </Route>
+
+          <Route element={<AppLayoutDashboard />}>
+            <Route path="dashboard" element={<DashboardHome />} />
+            <Route path="dashboard/article" element={<DashboardArticle />} />
+            <Route path="dashboard/category" element={<DashboardCategory />} />
           </Route>
         </Routes>
       </BrowserRouter>
