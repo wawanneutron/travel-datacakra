31e91a26f704f6f466c7bb64464f6ab5d8a6f528
wawanneutron <wawanneutron1331@gmail.com>
2025-09-03 09:23:41 +0700

    delete unnecessary code

--- src/styles/CardStyles.ts
+++
@@ -1,7 +0,0 @@
-import { css } from 'styled-components'
-
-export const box = css`
-  background-color: var(--color-grey-0);
-  border: 1px solid var(--color-grey-100);
-  border-radius: var(--border-radius-md);
-`
