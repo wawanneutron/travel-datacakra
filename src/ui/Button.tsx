c9f521adeddd30d1e78a3767fd747f2b89127a5f
wawanneutron <wawanneutron1331@gmail.com>
2025-09-04 22:11:56 +0700

    feat: button reus

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,8 +1,9 @@
 import type { ButtonProps } from '../types/ui'
 
-function Button({ children, disabled = false }: ButtonProps) {
+function Button({ children, onClick, disabled = false }: ButtonProps) {
   return (
     <button
+      onClick={onClick}
       className="bg-accent-500 disabled:bg-accent-800 rounded-full px-8 py-4 mt-6 text-primary-700 font-mono text-lg disabled:text-primary-400 font-semibold hover:bg-accent-600 transition-all w-full flex items-center justify-center gap-4"
       disabled={disabled}
     >
