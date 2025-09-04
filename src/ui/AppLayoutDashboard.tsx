7ee5e835f6c374e341f7d78041c6b085cc3b9ca7
wawanneutron <wawanneutron1331@gmail.com>
2025-09-04 10:51:13 +0700

    refactor (layout dashboard): add overflow

--- src/ui/AppLayoutDashboard.tsx
+++ src/ui/AppLayoutDashboard.tsx
@@ -6,8 +6,10 @@ function AppLayoutDashboard() {
   return (
     <aside className="grid grid-cols-[16rem_1fr] h-full gap-12 bg-primary-950 text-primary-100">
       <SideNavigation />
-      <main className="h-screen">
-        <Header />
+      <main className="h-screen overflow-auto">
+        <div className="px-4 py-2">
+          <Header />
+        </div>
         <Outlet />
       </main>
     </aside>
