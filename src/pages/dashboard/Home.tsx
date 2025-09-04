9d9f5228f97360d122cd5ac645b9db3e28272ccc
wawanneutron <wawanneutron1331@gmail.com>
2025-09-04 10:51:44 +0700

    feat: show auth username on dashboard

--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -1,11 +1,19 @@
+import { useSelector } from 'react-redux'
+import { getUser } from '../../features/auth/authSlice'
+
 export default function DashboardHome() {
+  const user = useSelector(getUser)
+
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Dashboard Home</h1>
-      <p>
-        Welcome to the dashboard! Here you can manage your articles and
-        categories.
-      </p>
+    <div className="py-2">
+      <div className="border-b border-primary-800 pb-2">
+        <h1 className="text-2xl font-bold mb-2">Dashboard</h1>
+        <p>
+          Welcome{' '}
+          <span className="text-accent-500 capitalize">{user?.username}</span>{' '}
+          to the dashboard! 👋
+        </p>
+      </div>
     </div>
   )
 }
