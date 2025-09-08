d22d24dfa09ab61fdccadaa94f84f2194414f915
wawanneutron <wawanneutron1331@gmail.com>
2025-09-08 22:04:10 +0700

    enhance: delete text

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -3,11 +3,6 @@ import DetailTravelArticle from '../features/travel/DetailTravelArticle'
 export default function Detail() {
   return (
     <div className="my-4">
-      <h1 className="text-accent-500 text-3xl font-mono">Detail Article</h1>
-      <p className="text-gray-400 max-w-3xl italic">
-        From mountain adventures to seaside escapes
-      </p>
-
       <DetailTravelArticle />
     </div>
   )
