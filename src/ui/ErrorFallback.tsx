0e541b87fae75126395ae03bd6d16138332c1d57
wawanneutron <wawanneutron1331@gmail.com>
2025-09-08 22:05:10 +0700

    enhance: ui error

--- src/ui/ErrorFallback.tsx
+++ src/ui/ErrorFallback.tsx
@@ -1,18 +1,16 @@
 import type { FallbackProps } from 'react-error-boundary'
+import Button from './Button'
 
 function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
-    <section className="p-6 text-center">
+    <section className="flex flex-col justify-center bg-primary-950 min-h-screen p-6 text-center">
       <h1 className="text-xl font-semibold text-red-600">
         Upss.. something went wrong 😔
       </h1>
-      <p className="mt-2 text-gray-700">{error.message}</p>
-      <button
-        onClick={resetErrorBoundary}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg"
-      >
-        Try again
-      </button>
+      <p className="mt-2 text-accent-700">{error.message}</p>
+      <div className="max-w-64 mx-auto">
+        <Button onClick={resetErrorBoundary}>Try again</Button>
+      </div>
     </section>
   )
 }
